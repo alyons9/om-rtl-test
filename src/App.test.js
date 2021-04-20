0d6a00899eee2b149d9ea0a4567b96f909a09fdb
Anthony Lyons <anthony_lyons@homedepot.com>
2021-04-20 12:59:36 -0400

    refactor

--- src/App.test.js
+++ src/App.test.js
@@ -8,41 +8,41 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+// test('find list elements', () => {
+
+//   render(<App />);
+
+//   const listByText = screen.getByText(/Harry POTter/i);
+//   expect(listByText.textContent).toBe("Harry potter");
+
+//   // const list = screen.getByRole("list");
+//   // const { getAllByRole } = within(list);
+//   // const items = getAllByRole("listitem")
+//   // expect(items.length).toBe(3);
+
+//   const listUsingAriaLabel = screen.getByRole("list", {name: /books/i});
+//   const { getAllByRole } = within(listUsingAriaLabel);
+//   const items = getAllByRole("listitem");
+//   expect(items.length).toBe(3);
+
+// });
+
 test('find list elements', () => {
 
   render(<App />);
 
-  const listByText = screen.getByText(/Harry POTter/i);
-  expect(listByText.textContent).toBe("Harry potter");
-
-  // const list = screen.getByRole("list");
-  // const { getAllByRole } = within(list);
-  // const items = getAllByRole("listitem")
-  // expect(items.length).toBe(3);
-
-  const listUsingAriaLabel = screen.getByRole("list", {name: /books/i});
-  const { getAllByRole } = within(listUsingAriaLabel);
-  const items = getAllByRole("listitem");
-  expect(items.length).toBe(3);
-
-});
-
-test('find list elements', () => {
-
-  render(<App />);
-
-  // const listByText = screen.getByText(/1/i);
+  // const listByText = screen.getByText(/Harry POTter/i);
   // expect(listByText).toBeInTheDocument();
   // expect(listByText.textContent).toBe("1");
 
-  // const list = screen.getByRole("list");
-  // const { getAllByRole } = within(list);
-  // const items = getAllByRole("listitem")
-  // expect(items.length).toBe(3);
-
-  const listUsingAriaLabel = screen.getByRole("list", {name: /books/i});
-  const { getAllByRole } = within(listUsingAriaLabel);
-  const items = getAllByRole("listitem");
+  const list = screen.getByRole("list");
+  const { getAllByRole } = within(list);
+  const items = getAllByRole("listitem")
   expect(items.length).toBe(3);
+
+  // const listUsingAriaLabel = screen.getByRole("list", {name: /books/i});
+  // const { getAllByRole } = within(listUsingAriaLabel);
+  // const items = getAllByRole("listitem");
+  // expect(items.length).toBe(3);
 
 });
